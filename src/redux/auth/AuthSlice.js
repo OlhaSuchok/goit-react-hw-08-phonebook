4d62c0c348a3edd386407abb5f9de4c461138259
Olha-Suchok <suchok_olya@ukr.net>
2023-01-25 16:31:30 +0200

    Refactored code

--- src/redux/auth/AuthSlice.js
+++ src/redux/auth/AuthSlice.js
@@ -6,11 +6,24 @@ import {
   fetchCurrentUser,
 } from 'redux/auth/Auth-operations';
 
+const handlePending = state => {
+  state.isLoggedIn = false;
+  state.isLoading = true;
+  state.error = null;
+};
+
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
+
 const initialState = {
   user: { name: '', email: '' },
   token: null,
   isLoggedIn: false,
   isFetchingCurrentUser: false,
+  isLoading: false,
+  error: null,
 };
 
 const authSlice = createSlice({
@@ -18,25 +31,29 @@ const authSlice = createSlice({
   initialState: initialState,
   extraReducers: builder => {
     builder
+      .addCase(register.pending, handlePending)
       .addCase(register.fulfilled, (state, action) => {
         state.user = action.payload.user;
         state.token = action.payload.token;
         state.isLoggedIn = true;
       })
-      .addCase(register, (state, { error }) => {
-        state.error = error.message;
-      })
+      .addCase(register.rejected, handleRejected)
+      .addCase(logIn.pending, handlePending)
       .addCase(logIn.fulfilled, (state, action) => {
         state.user = action.payload.user;
         state.token = action.payload.token;
         state.isLoggedIn = true;
       })
-      .addCase(logOut.fulfilled, (state, action) => {
+      .addCase(logIn.rejected, handleRejected)
+      .addCase(logOut.pending, handlePending)
+      .addCase(logOut.fulfilled, state => {
         state.user = { name: '', email: '' };
         state.token = null;
+        state.isLoading = false;
         state.isLoggedIn = false;
       })
-      .addCase(fetchCurrentUser.pending, (state, action) => {
+      .addCase(logOut.rejected, handleRejected)
+      .addCase(fetchCurrentUser.pending, state => {
         state.isFetchingCurrentUser = true;
       })
       .addCase(fetchCurrentUser.fulfilled, (state, action) => {
@@ -44,7 +61,7 @@ const authSlice = createSlice({
         state.isLoggedIn = true;
         state.isFetchingCurrentUser = false;
       })
-      .addCase(fetchCurrentUser.rejected, (state, action) => {
+      .addCase(fetchCurrentUser.rejected, state => {
         state.isFetchingCurrentUser = false;
       });
   },
